8ce651211cb1aba452a3a725e0c7580836eab26b
Regis Ribeiro <regisgomesr@gmail.com>
2020-03-10 12:55:34 -0300

    Estilização

---
+++ src/components/Card/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container, Label } from './styles';
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label color='#7159c1'/>
+      </header>
+      <p>Fazer a migração completa do servidor</p>
+      <img src='https://api.adorable.io/avatars/285/abott@adorable.png' alt='' />
+
+    </Container>
+  );
+}
